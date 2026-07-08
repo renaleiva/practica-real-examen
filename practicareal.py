377fdf5a1861f6f91d36243e1b10b99f74396fbf
renaleiva <rena.leiva@duocuc.cl>
2026-07-08 16:14:47 -0400

    Validamos que el año de la cancion sea mayor a 1950 y la duracion este entre 0.5 y 15.0

--- practicareal.py
+++ practicareal.py
@@ -6,4 +6,29 @@ def validar_titulo():
         else:
             return titulo
         
+def validar_anio():
+    while True:
+        try:
+            anio_numero = int(input("Ingrese el año de lanzamiento de la cancion: "))
+            if anio_numero > 1950:
+                return anio_numero
+            else:
+                print("El año debe ser mayor a 1950")
+        except ValueError:
+            print("Error: Debe ingresar un numero entero valido para el año")
+
+def validar_duracion():
+    while True:
+        try:
+            duracion = float(input("Ingrese la duracion de la cancion: "))
+            if 0.5 <= duracion <= 15.0:
+                return duracion
+            else:
+                print("La duracion debe estar entre 05 y 15.0")
+        except ValueError:
+            print("El numero debe ser decimal valido.")        
+
+
+    
+
         
